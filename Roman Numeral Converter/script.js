2ef2d7fe774dde75e44ff5b5d7d62d6d91999ba9
Angelica Sebastian <89622698+angelica1828@users.noreply.github.com>
2024-03-16 11:47:46 +0530

    Update script.js

--- Roman Numeral Converter/script.js
+++ Roman Numeral Converter/script.js
@@ -1,22 +1,64 @@
-const convertBtn = document.getElementById('convert-btn')
-const output = document.getElementById('output')
+const convertBtn = document.getElementById('convert-btn');
+const output = document.getElementById('output');
+
+const checkEmpty = (str) =>{
+    return str==="";
+}
+
+const checkConstraintsLower = (ipInt) =>{
+  return ipInt<0;
+}
+
+const checkConstraintsUpper = (ipInt) =>{
+  return ipInt>=4000;
+}
+
+const convertToroman = (decimalNum) =>{
+  const romanSymbols = [
+    { value: 1000, symbol: 'M' }, { value: 900, symbol: 'CM' },
+    { value: 500, symbol: 'D' }, { value: 400, symbol: 'CD' },
+    { value: 100, symbol: 'C' }, { value: 90, symbol: 'XC' },
+    { value: 50, symbol: 'L' }, { value: 40, symbol: 'XL' },
+    { value: 10, symbol: 'X' }, { value: 9, symbol: 'IX' },
+    { value: 5, symbol: 'V' }, { value: 4, symbol: 'IV' },
+    { value: 1, symbol: 'I' }
+];
+
+let romanNumeral = '';
+
+for (let i = 0; i < romanSymbols.length; i++) {
+    while (decimalNum >= romanSymbols[i].value) {
+        romanNumeral += romanSymbols[i].symbol;
+        decimalNum -= romanSymbols[i].value;
+    }
+}
+return romanNumeral
+}
 
 const checks = () =>{
   const inputNum = document.getElementById('number');
   const intInput =  inputNum.value;
-  if(intInput===""){
-    output.innerText = "Please enter a valid number"
+
+  if(checkEmpty(intInput)){
+    output.innerText = "Please enter a valid number";
+    return;
   }
-  else if(intInput<0){
-    output.innerText = "Please enter a number greater than or equal to 1"
+  else if(checkConstraintsLower(intInput)){
+    output.innerText = "Please enter a number greater than or equal to 1";
+    return;
   }
-  else if(intInput>=4000){
-    output.innerText = "Please enter a number less than or equal to 3999"
+  else if(checkConstraintsUpper(intInput)){
+    output.innerText = "Please enter a number less than or equal to 3999";
+    return;
   }
+  opText = convertToroman(intInput);
+  output.innerText = opText;
 }
+
 
 const displayOutput = () =>{
     checks()
+    output.innerText = "hello bois"
 }
 
-convertBtn.addEventListener("click",displayOutput)
+convertBtn.addEventListener("click",checks)
