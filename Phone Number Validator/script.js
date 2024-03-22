be6911035268e0b85512b2fcb01a0a42134f5148
Angelica Sebastian <89622698+angelica1828@users.noreply.github.com>
2024-03-22 10:30:50 +0530

    Create script.js

---
+++ Phone Number Validator/script.js
@@ -0,0 +1,33 @@
+const checkButton = document.getElementById('check-btn');
+const clearButton = document.getElementById('clear-btn');
+const result = document.getElementById('results-div');
+
+const validate = () =>{
+    const userInput = document.getElementById('user-input');
+
+    if(userInput.value === ""){
+        alert("Please provide a phone number");
+        return
+    }
+    const regex = /^1?\s?(?:\(\d{3}\)|\d{3})[-\s]?\d{3}[-\s]?\d{4}$/g;
+    // ^1?\s? == matching beginning 1 and space after 1, optional
+    // (?:\(\d{3}\)|\d{3}) == non capturing group which matches (123) or 123
+    // [-\s]? == group which matches '-' or ' '
+    if(regex.test(userInput.value)){
+        result.textContent = `Valid US number: ${userInput.value}`;
+    }
+    else{
+        result.textContent = `Invalid US number: ${userInput.value}`;
+    }
+    
+}
+
+const displayOutput = () =>{
+}
+
+checkButton.addEventListener("click",validate);
+
+clearButton.addEventListener("click",()=>{
+    const userInput = document.getElementById('user-input');
+    result.textContent = "";
+})
